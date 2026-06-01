dc1d96ade7ee703c43b5bd952f4b82bd34cb4241
aceassured <bharath@aceassured.com>
2026-06-01 19:37:05 +0530

    first commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import 'dotenv/config';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
+
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    const adapter = new PrismaPg({
+      connectionString,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
